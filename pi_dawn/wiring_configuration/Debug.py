b4eba37bee22e830c99d01a1813016c07243cbc9
Janik Stracke <j.stracke@itsmind.com>
2022-12-06 21:47:12 +0100

    Add Debug WiringConfiguration

---
+++ pi_dawn/wiring_configuration/Debug.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+from pi_dawn.hw import Hardware
+from pi_dawn.wiring_configuration.WiringConfigurationDecorator import WiringConfigurationDecorator
+
+class Debug(WiringConfigurationDecorator):
+    def __init__(self, hardware: Hardware = None, debug: bool = os.environ.get('DEBUG', '0') == '1'):
+        super().__init__(hardware)
+        self.debug = debug
+        self.state = StateDebug(self.hardware) if debug else StateProduction(self.hardware)
+
+    def start_refresh(self):
+        self.state.start_refresh()
+
+    def refresh(self):
+        self.state.refresh()
+
+    def set_pixel(self, screen, pixel, color):
+        self.state.set_pixel(screen, pixel, color)
+
+class StateDebug:
+    def __init__(self, hardware):
+        self.hardware = hardware
+
+    def start_refresh(self):
+        pass
+
+    def refresh(self):
+        pass
+
+    def set_pixel(self, screen, pixel, color):
+        self.hardware.start_refresh()
+        self.hardware.set_pixel(screen, pixel, color)
+        self.hardware.refresh()
+
+        time.sleep(0.05)
+
+class StateProduction:
+    def __init__(self, hardware):
+        self.hardware = hardware
+
+    def start_refresh(self):
+        self.hardware.start_refresh()
+
+    def refresh(self):
+        self.hardware.refresh()
+
+    def set_pixel(self, screen, pixel, color):
+        self.hardware.set_pixel(screen, pixel, color)
--- pi_dawn/wiring_configuration/__init__.py
+++ pi_dawn/wiring_configuration/__init__.py
@@ -1,4 +1,5 @@
 from pi_dawn.wiring_configuration.Alternate import Alternate
+from pi_dawn.wiring_configuration.Debug import Debug
 from pi_dawn.wiring_configuration.Stripe import Stripe
 
-__ALL__ = [Alternate, Stripe]
+__ALL__ = [Alternate, Debug, Stripe]
